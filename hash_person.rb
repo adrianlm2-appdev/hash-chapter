fd6d10f285776b853b70efac016e462e88986559
adrianlm2 <adrianl0@chicagobooth.edu>
2022-01-22 18:46:06 +0000

    frustrating!

--- hash_person.rb
+++ hash_person.rb
@@ -1,9 +1,9 @@
 # Ask for a name, age, and occupation; store the values in a Hash; display the Hash.
 
-p "Enter a name, age, and occupation separated by spaces:\n"
+p "Enter a name, age, and occupation separated by spaces:"
 
 info = gets.chomp.split
 
 person1 = { :name => info[0], :age => info[1], :occupation => info[2] }
 
-p person1 
+p person1
